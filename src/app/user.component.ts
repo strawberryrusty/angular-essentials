4567c504799514d53abb3e3f740b0e3a567e7997
Prashanth Mohan <prashmohanuon@gmail.com>
2022-04-28 15:36:24 +0100

    21. Binding to properties

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -3,14 +3,14 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-user',
   template:`
-  <input type="text"(input)="onUserInput($event)">
+  <input type="text"(input)="onUserInput($event)" [value]="name">
   <p>Hello {{name}}!</p>
    <p>Hello</p>
 `
 })
 
 export class UserComponent {
-  name = 'Prash';
+  name = 'Prashanth';
 
   onUserInput(event: any) {
     this.name = event.target.value;
